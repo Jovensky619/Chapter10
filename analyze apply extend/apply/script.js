31a72ed0820b8dd6a3ff3c856ea54c22a6a24df9
Jovensky Siant-Hilaire <js137@sunyorange.edu>
2022-11-17 10:18:16 -0500

    finished

--- analyze apply extend/apply/script.js
+++ analyze apply extend/apply/script.js
@@ -6,8 +6,8 @@
 
 /* Function to display content */
 function content() {
-var text =  document.getElementyById("new");
-text.textContent = "Hi. I am a new paragrapgh that was created with a JavaScript funvtion.";
+var text =  document.getElementById("new");
+text.innerHTML = "Hi. I am a new paragraph that was created with a JavaScript function.";
 text.style.color = "#c0145f"
 text.style.fontSize = "2em";
 }

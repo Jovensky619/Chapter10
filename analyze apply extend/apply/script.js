dde6069e9e7125cae70787e3d8704bf9b8870dac
Jovensky Siant-Hilaire <js137@sunyorange.edu>
2022-11-17 09:22:11 -0500

    added

---
+++ analyze apply extend/apply/script.js
@@ -0,0 +1,13 @@
+/*
+    Student Name: Jovensky Sainthilaire
+	File Name: apply	
+	Current Date: 11/16/22
+*/
+
+/* Function to display content */
+function content() {
+var text =  document.getElementyById("new");
+text.textContent = "Hi. I am a new paragrapgh that was created with a JavaScript funvtion.";
+text.style.color = "#c0145f"
+text.style.fontSize = "2em";
+}
